e7e56755cb6b32a90bc05dea75a8f507ea490baa
Juliusz Gonera <juliusz.gonera@gmail.com>
2011-04-14 19:19:56 +0200

    Added code from graham lisp book excercises

---
+++ Lisp/graham-book.lisp
@@ -0,0 +1,46 @@
+; Write a version of union that preserves the order of the elements in
+; the original lists:
+; > (new-union '(a b c) '(b a d))
+; (A B C D)
+
+
+; A BST tree implementation
+(defstruct (node (:print-function
+                   (lambda (n s d)
+                     (format s "#<~A>" (node-elt n)))))
+  elt (l nil) (r nil))
+
+(defun bst-insert (obj bst <)
+  (if (null bst)
+      (make-node :elt obj)
+      (let ((elt (node-elt bst)))
+        (if (eql obj elt)
+            bst
+            (if (funcall < obj elt)
+                (make-node
+                  :elt elt
+                  :l   (bst-insert obj (node-l bst) <)
+                  :r   (node-r bst))
+                (make-node
+                  :elt elt
+                  :r   (bst-insert obj (node-r bst) <)
+                  :l   (node-l bst)))))))
+
+
+(defun bst-find (obj bst <)
+  (if (null bst)
+      nil
+      (let ((elt (node-elt bst)))
+        (if (eql obj elt)
+            elt
+            (if (funcall < obj elt)
+                (bst-find obj (node-l bst) <)
+                (bst-find obj (node-r bst) <))))))
+
+(defun bst-min (bst)
+  (and bst
+       (or (bst-min (node-l bst)) bst)))
+
+(defun bst-max (bst)
+  (and bst
+       (or (bst-max (node-r bst)) bst)))
